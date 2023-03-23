cbd4817963078add8d890471e88c657afd747981
Guillermo Alejandro Costilla <gcostilla96@gmail.com>
2023-03-23 11:53:56 -0300

    correcciones y stats

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,71 +1,123 @@
 let urlApi = 'https://mindhub-xj03.onrender.com/api/amazing';
 
 let eventos;
-let data;
+let categorys = [];
+let upEventos = [];
+let pasado = [];
 
 async function getEvents() {
     try {
         let response = await fetch(urlApi);
         let dataApi = await response.json();
-
-        eventos = dataApi.events;
-        data = dataApi.currentDate;
-
-        /* pastEvents(eventos, data);
-        upEvents(eventos, data); */
-        extractCategory(eventos)
-
+        eventos = dataApi.events
+        printStats(eventos)
+        let categorys = extractCategory(eventos);
+        upEventos = upEvents(eventos, dataApi.currentDate)
+        pasado = pastEvents(eventos, dataApi.currentDate)
+        
+        eventosPorAsistencia(upEventos, pasado)
     } catch (error) {
-        console.log(error.message);
+        console.log(error.message)
     }
 }
 
 getEvents();
 
-/* function pastEvents(eventos, data) {
-    let añopasado = []
-    for (let item of eventos) {
-        if (item.date < data) {
-            añopasado.push(item)
-        }
-    }
-    resultado = 0;
-    resultado = arr.assistance * 100 / arr.capacity + "%";
+// saco los porcentajes de asistencia
+function porcentajes(data) {
+    let porcentaje = (data.assistance * 100 / data.capacity)
+    return porcentaje
 }
-
-
-function upEvents(eventos, data) {
-    let añofuturo = []
-    for (let item of eventos) {
-        if (item.date > data) {
-            añofuturo.push(item)
-        }
-    }
-    resultado = 0;
-    resultrado = arr.estimate * 100 / arr.capacity + "%";
-} */
-
-
-/* function getBigger(pokemons) {
-    return pokemons.reduce((acumulador, valorActual) => {
-        if (valorActual.height > acumulador.height) {
+// evento con mas asistencia
+function masAsistencia(arr) {
+    return arr.reduce((acumulador, valorActual) => {
+        if (porcentajes(valorActual) > porcentajes(acumulador)) {
             return valorActual;
         } else {
-            return acumulador;
+            return acumulador
         }
-    });
-} */
-/* 
-Math.min(resultado = 0
-        resultado = evento.estimate * 100 / evento.capacity + "%") */
+    })
+}
+//evento con menor asistencia
+function menorAsitencia(arr) {
+    return arr.reduce((acumulador, valorActual) => {
+        if (porcentajes(valorActual) < porcentajes(acumulador)) {
 
-function extractCategory(eventos) {
-    let categorias = [];
-    eventos.forEach(evento => {
-            if (categorias.includes(evento.category)) {
-                categorias.push(evento);
+            return valorActual;
+        } else {
+            return acumulador
+        }
+    })
+}
+// evento con mayor capacidad
+function capacidades(arr) {
+    return arr.reduce((acumulador, valorActual) => {
+        if (valorActual.capacity > acumulador.capacity) {
+
+            return valorActual;
+        } else {
+            return acumulador
+        }
+    })
+}
+//impresion de stats
+function printStats(arr) {
+    let container = document.querySelector(".primario");
+    let masC = masAsistencia(arr);
+    let menosC = menorAsitencia(arr);
+    let capacidad = capacidades(arr);
+    container.innerHTML += `
+        <tr>
+            <td class="td">${masC.name}:  ${(porcentajes(masC)).toFixed(2)}% </td>
+            <td class="td">${menosC.name}:  ${(porcentajes(menosC)).toFixed(2)}% </td>
+            <td class="td">${capacidad.name}:  ${capacidad.capacity} </td>
+        </tr>`
+}
+///////////////////////////////////////////////////////////////////////////
+// 2da Parte Upcoming Events Stats //
+
+function extractCategory(arr) {
+    let categorys = [];
+    arr.forEach(elemento => {
+        if (!categorys.includes(elemento.category)) {
+            categorys.push(elemento.category);
+        }
+    }); return categorys;
+};
+
+function upEvents(eventos, data) {
+    let upEventos = []
+    for (item of eventos) {
+        if (item.date > data) {
+            upEventos.push(item)
+
+        }
+    } return upEventos;
+}
+
+function pastEvents(eventos, data) {
+    let pasado = []
+    for (item of eventos) {
+        if (item.date < data) {
+            pasado.push(item)
+
+        }
+    } return pasado;
+}
+
+ function futuro(arr,categorias) {
+     let ganancias = [];
+     let acumulador = 0;
+    arr.forEach(evento => {
+        categorias.forEach(categoria => {
+            if (evento.category == categoria) {
+                acumulador += evento.price * evento.estimate 
+                ganancias.push(acumulador) 
+            }else{
+                console.log('la categoria del evento no')
             }
-        }); console.log(categorias);
-    };
+        })
+    }); console.log(ganancias) 
     
-      eventos.filter(evento.category)
+} 
+
